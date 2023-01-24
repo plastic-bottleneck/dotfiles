49d44c61e7c6e3e10ed4146f4ed0a50ad917eabe
Paul <99998358+plastic-bottleneck@users.noreply.github.com>
2023-01-24 17:27:53 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash/
+
+# Made by Plastic bottleneck (Paul)
+# https://github.com/plastic-bottleneck/
+
+echo
+echo // Making Directories //
+echo
+mkdir ~/.config ~/.config/alacritty ~/.config/bspwm ~/pix ~/pix/wal
+echo
+echo // About to copy files //
+echo
+cp .zshrc ~/
+cp alacritty.yml ~/.config/alacritty/
+cp bspwmrc ~/.config/bspwm/
+cp wallpaper.jpg ~/pix/wal/
+echo
+echo [ Done ]
+echo
+echo Made by Paul alias Plastic Bottleneck
+echo GitHub: https://github.com/plastic-bottleneck
+echo
+echo To use the wallpaper, use a program of your choice for you.
+echo The wallpaper is located on ~/pix/wal/ called wallpaper.jpg
+echo
